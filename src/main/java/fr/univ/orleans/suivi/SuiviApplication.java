c7f59fa7cc52f8f50a2055922cf4cbe5be32cce9
Bah Amadou Mouctar <amadou-mouctar.bah@etu.univ-orleans.fr>
2021-04-20 22:16:35 +0200

    Initial commit

---
+++ src/main/java/fr/univ/orleans/suivi/SuiviApplication.java
@@ -0,0 +1,13 @@
+package fr.univ.orleans.suivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univ/orleans/suivi/SuiviApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ.orleans.suivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
