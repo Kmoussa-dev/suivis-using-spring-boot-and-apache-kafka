ae4915e3f69f14518ee96b6ae15ed7ee561fc3a6
kmoussa <moussa.kone@etu.univ-orleans.fr>
2021-05-15 20:04:19 +0200

    config debugger

---
+++ demande-events-producer/src/main/java/com/projetkafka/DemandeEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.projetkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandeEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandeEventsProducerApplication.class, args);
+	}
+
+}
---
+++ demande-events-producer/src/test/java/com/projetkafka/DemandeEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandeEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
