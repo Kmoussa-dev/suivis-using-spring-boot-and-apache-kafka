349295a540b79afe9f223f7cba97f14b70bb6d5a
kmoussa <moussa.kone@etu.univ-orleans.fr>
2021-05-15 20:23:58 +0200

    mis à jour controller

--- demande-events-producer/src/main/java/com/projetkafka/controller/DemandeEventsController.java
+++ demande-events-producer/src/main/java/com/projetkafka/controller/DemandeEventsController.java
@@ -1,7 +1,10 @@
 package com.projetkafka.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.projetkafka.domain.DemandeEvent;
+import com.projetkafka.producer.DemandeEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemandeEventsController {
 
+    @Autowired
+    DemandeEventProducer demandeEventProducer;
+
     @PostMapping("/v1/demandeevent")
-    public ResponseEntity<DemandeEvent> postDemandeEvent(@RequestBody DemandeEvent demandeEvent){
+    public ResponseEntity<DemandeEvent> postDemandeEvent(@RequestBody DemandeEvent demandeEvent) throws JsonProcessingException {
 
         //invoke kafka producer
+        demandeEventProducer.sendDemandeEvent(demandeEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(demandeEvent);
     }
 }
