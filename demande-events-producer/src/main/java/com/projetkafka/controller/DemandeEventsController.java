1d5127e8bcb8cd4dba932b6782dfeffcc49fdade
kmoussa <moussa.kone@etu.univ-orleans.fr>
2021-05-16 20:25:40 +0200

    deuxième approche envoi de message

--- demande-events-producer/src/main/java/com/projetkafka/controller/DemandeEventsController.java
+++ demande-events-producer/src/main/java/com/projetkafka/controller/DemandeEventsController.java
@@ -4,24 +4,34 @@ package com.projetkafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.projetkafka.domain.DemandeEvent;
 import com.projetkafka.producer.DemandeEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @RestController
+@Slf4j
 public class DemandeEventsController {
 
     @Autowired
     DemandeEventProducer demandeEventProducer;
 
     @PostMapping("/v1/demandeevent")
-    public ResponseEntity<DemandeEvent> postDemandeEvent(@RequestBody DemandeEvent demandeEvent) throws JsonProcessingException {
+    public ResponseEntity<DemandeEvent> postDemandeEvent(@RequestBody DemandeEvent demandeEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
 
         //invoke kafka producer
-        demandeEventProducer.sendDemandeEvent(demandeEvent);
+        log.info("before sendDemandeEvent");
+        // demandeEventProducer.sendDemandeEvent(demandeEvent);
+        SendResult<Integer, String> sendResult = demandeEventProducer.sendDemandeEventSynchronous(demandeEvent);
+        log.info("SendResult is {} ", sendResult.toString());
+        log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(demandeEvent);
     }
 }
--- demande-events-producer/src/main/java/com/projetkafka/producer/DemandeEventProducer.java
+++ demande-events-producer/src/main/java/com/projetkafka/producer/DemandeEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 public class DemandeEventProducer {
@@ -57,6 +61,24 @@ public class DemandeEventProducer {
         log.info("Message Sent SuccessFully for the key : {} and the value is {} , partition is {}", key, value, result.getRecordMetadata().partition());
     }
 
+    public SendResult<Integer, String> sendDemandeEventSynchronous(DemandeEvent demandeEvent) throws ExecutionException, InterruptedException, JsonProcessingException, TimeoutException {
+
+        Integer key = demandeEvent.getDemandeEventId();
+        String value = objectMapper.writeValueAsString(demandeEvent);
+        SendResult<Integer,String> sendResult=null;
+        try {
+            sendResult = kafkaTemplate.sendDefault(key,value).get(1, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("ExecutionException/InterruptedException Sending the Message and the exception is {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Exception Sending the Message and the exception is {}", e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
+
+    }
 }
 
 
